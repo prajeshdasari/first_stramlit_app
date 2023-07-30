80dcaca7a22011f57842befd993a48080e9a417d
prajeshdasari <prajesh.dasari@trianz.com>
2023-07-30 19:38:24 -0400

    added few more lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
 import streamlit
 streamlit.title ('My parents New Healthy Diner')
-streamlit.text ('Breakfast Menu')
+streamlit.header ('Breakfast Menu')
+streamlit.text ('Omega 3 & Blueberry Oatmeal')
