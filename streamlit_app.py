af544a458ec7c3fa469634dc44173b8b844e0ffd
prajeshdasari <prajesh.dasari@trianz.com>
2023-07-30 19:40:26 -0400

    added other menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,5 @@ import streamlit
 streamlit.title ('My parents New Healthy Diner')
 streamlit.header ('Breakfast Menu')
 streamlit.text ('Omega 3 & Blueberry Oatmeal')
-streamlit.text ('Other healthy items')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
